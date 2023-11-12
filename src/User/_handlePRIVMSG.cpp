cb600b8c9c3616ba704ea6b6aa03ee913c95d220
beroux <42benoit.roux@gmail.com>
2023-11-12 23:28:19 +0100

    style(commands): Vince the linter check the code
    Nothing to said

--- src/User/_handlePRIVMSG.cpp
+++ src/User/_handlePRIVMSG.cpp
@@ -3,29 +3,38 @@
 #include "Server.hpp"
 #include "User.hpp"
 
-static std::string  constructMessageToUser(const User& sender, const User& receiver, const std::string& body);
+static std::string  constructMessageToUser(const User& sender,
+                                           const User& receiver,
+                                           const std::string& body);
 
 void User::_handlePRIVMSG(Server&server, const std::vector<std::string>&args) {
     std::vector<std::string>    targets;
     User*                       currTarget;
 
     if (args.size() < 2) {
-        _sendMessage(NumericReplies::Error::needMoreParameters(_nickName, "PRIVMSG"), server);
+        _sendMessage(NumericReplies::Error::needMoreParameters(_nickName, "PRIVMSG"),
+                     server);
     }
     targets = ft::String::split(args[0], ",");
-    for (std::vector<std::string>::const_iterator it = targets.begin(); it != targets.end(); ++it) {
+    for (std::vector<std::string>::const_iterator it = targets.begin();
+         it != targets.end();
+         ++it) {
         currTarget = server.getUserByNickname(*it);
         if (currTarget) {
-            ft::Log::info << "send :" << args[1] << " to " << currTarget->getNickName() << std::endl;
-            currTarget->_sendMessage(constructMessageToUser(*this, *currTarget, args[1]),
-                                    server);
+            currTarget->_sendMessage(constructMessageToUser(*this,
+                                                            *currTarget,
+                                                            args[1]),
+                                     server);
         }
     }
 }
 
-static std::string  constructMessageToUser(const User& sender, const User& receiver, const std::string& body) {
+static std::string  constructMessageToUser(const User& sender,
+                                           const User& receiver,
+                                           const std::string& body) {
     std::stringstream   message;
 
-    message << ":" << sender.getNickName() << " PRIVMSG " << receiver.getNickName() << " :" << body << "\r\n";
+    message << ":" << sender.getNickName() << " PRIVMSG " <<
+            receiver.getNickName() << " :" << body << "\r\n";
     return (message.str());
 }
