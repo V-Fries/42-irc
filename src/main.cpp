7ea884f1864686d88417b611527a2cc9226a5cd1
V-Fries <vincentfries@proton.me>
2023-11-15 04:19:08 +0100

    Port is now parsed from arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,51 @@
 #include "SignalHandler.hpp"
 #include "ft.hpp"
 
+#include <limits>
+#include <cerrno>
+#include <cstdlib>
 #include <iostream>
 
+static uint16_t getPort(const std::string& portStr);
+
 int main(int argc, char** argv) {
-    static_cast<void>(argc); // TODO remove me
-    static_cast<void>(argv); // TODO remove me
     ft::Log::setDebugLevel(ft::Log::DEBUG);
     ft::Log::setFileToWriteTo("logs.txt");
 
-    User::initRequestsHandlers();
+    if (argc != 3) {
+        ft::Log::critical << "Wrong number of arguments, expected:\n"
+                             "\t./ircserv PORT PASSWORD" << std::endl;
+        return 1;
+    }
 
     try {
-        Server  server(6667, "password"); // TODO Use args to set the server up
+        Server  server(getPort(argv[1]), argv[2]); // TODO check that the password in valid
+        User::initRequestsHandlers();
         SignalHandler::init(server);
         server.run();
     } catch (const ft::Exception& e) {
         e.printError();
+        return 2;
     }
+}
+
+static uint16_t getPort(const std::string& portStr) {
+    if (portStr.empty()) {
+        throw ft::Exception("Port argument should not be empty", ft::Log::CRITICAL);
+    }
+
+    char*       lastChar;
+    const long  port = std::strtol(portStr.c_str(), &lastChar, 10);
+
+    if (!std::isdigit(static_cast<unsigned char>(portStr[0])) || *lastChar != '\0') {
+        throw ft::Exception("Unexpected character in port argument: \"" + portStr + '\"',
+                            ft::Log::CRITICAL);
+    }
+    if (errno == ERANGE || port < 0 || port > std::numeric_limits<uint16_t>::max()) {
+        throw ft::Exception("Value for port should be an uint16",
+                            ft::Log::CRITICAL);
+    }
+
+    ft::Log::debug << "Port argument is " << static_cast<uint16_t>(port) << std::endl;
+    return static_cast<uint16_t>(port);
 }
