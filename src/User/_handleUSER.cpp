3f4863794ddadd3e5730e8b8d57b0a597040230e
V-Fries <vincentfries@proton.me>
2023-11-27 14:29:06 +0100

    Fixed hostname

--- src/User/_handleUSER.cpp
+++ src/User/_handleUSER.cpp
@@ -3,6 +3,12 @@
 #include "ft_String.hpp"
 #include "NumericReplies.hpp"
 
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+static std::string getRealName(int fd, const std::string& userSetRealName);
+
 void    User::_handleUSER(Server& server, const std::vector<std::string>& args) {
     ft::Log::info << "Received USER request: \"" << args << "\" from user " << _fd
                   << std::endl;
@@ -18,6 +24,22 @@ void    User::_handleUSER(Server& server, const std::vector<std::string>& args) 
 
     // TODO check behaviour when too many parameters are received
     _userName = '~' + args[0];
-    _realName = args[3];
+    _realName = ::getRealName(_fd, args[3]);
+    ft::Log::debug << "username: " << _userName << " hostname: " << _realName
+                   << std::endl;
     _registerUserIfReady(server);
+}
+
+static std::string getRealName(const int fd, const std::string& userSetRealName) {
+    struct sockaddr_in  addr = {};
+    socklen_t           len = (sizeof addr);
+    getsockname(fd, reinterpret_cast<struct sockaddr*>(&addr), &len);
+
+    const struct hostent* host = gethostbyname(inet_ntoa(addr.sin_addr));
+    if (host == NULL) {
+        ft::Log::warning << "gethostbyname failed with h error number: " << h_errno
+                         << std::endl;
+        return userSetRealName;
+    }
+    return host->h_name;
 }
