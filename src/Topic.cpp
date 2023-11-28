3fa079610bb3e4560e2b4c3fedbef928a5541608
beroux <beroux@student.42lyon.fr>
2023-11-28 09:48:18 +0100

    refactor(numeric replies): Remove noSuchNickname
    Remove the static method noSuchNickname because of the add of noSuchNick
    after the merge of main

--- src/Topic.cpp
+++ src/Topic.cpp
@@ -1,7 +1,8 @@
 #include "Topic.hpp"
 #include "ft.hpp"
 
-#include <cstring>
+#include <ctime>
+#include <sstream>
 
 Topic::Topic() {
 }
@@ -12,26 +13,12 @@ Topic::~Topic() {
 
 void Topic::setContent(const std::string& content,
                        const std::string& nickname) {
-    char    buffer[1024];
+    std::stringstream   time;
 
     _content = content;
     _nickname = nickname;
-
-    const int sinceEpochFd = open("/sys/class/rtc/rtc0/since_epoch", O_RDONLY);
-    if (sinceEpochFd == -1) {
-        ft::Log::error << "open /sys/class/rtc/rtc0/since_epoch file error: " << strerror(errno);
-        return;
-    }
-    ssize_t len = read(sinceEpochFd, buffer, 1024);
-    if (len == -1) {
-        close(sinceEpochFd);
-        ft::Log::error << "read /sys/class/rtc/rtc0/since_epoch file error: " << strerror(errno);
-        return;
-    }
-    if (len > 0 && buffer[len - 1] == '\n')
-        len--;
-    _setAt = std::string(buffer, len);
-    close(sinceEpochFd);
+    time << std::time(NULL);
+    _setAt = time.str();
 }
 
 const std::string& Topic::getContent() const {
